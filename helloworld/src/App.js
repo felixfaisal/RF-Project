4036f511134956c38558b7f193d2ea8cafbb545c
felixfaisal <faisalahmedfarooq46@gmail.com>
2020-10-02 13:22:16 +0530

    Added few things for the project

---
+++ helloworld/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import Greet from './components/Greet'
+import Welcome from './components/Welcome'
+
+function App() {
+  return (
+    <div>
+    <Greet name="Bruce"/>
+    <Welcome name="Bruce"/>
+    </div>
+  )
+}
+
+export default App;
---
+++ helloworld/src/components/Welcome.js
@@ -0,0 +1,9 @@
+
+import React, {Component} from 'react'
+class Welcome extends Component {
+    // eslint-disable-next-line
+    render(){
+    return(<h1>Well Well fuck off {this.props.name}</h1>)
+    }
+}
+export default Welcome
